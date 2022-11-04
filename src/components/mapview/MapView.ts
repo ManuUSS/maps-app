3549c6e5bc33aa76fad20495f12a7fa8c11c733e
Manuel Ulate <fr3zs09@gmail.com>
2022-11-04 11:57:51 -0600

    Implementación del marker con mi posicion actual

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,6 @@
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onMounted, watch } from 'vue';
 import { usePlacesStore } from '@/composables/usePlacesStore';
+import Mapboxgl from 'mapbox-gl';
 
 export default defineComponent({
     name: 'MapView',
@@ -8,8 +9,41 @@ export default defineComponent({
         const mapElement = ref<HTMLDivElement>();
         const { userLocation, isUserLocationReady } = usePlacesStore();
 
+        const initMap = async ( ) => {
+
+            await Promise.resolve();
+
+            const map = new Mapboxgl.Map({
+                container: mapElement.value!, // container ID
+                style: 'mapbox://styles/mapbox/light-v10', // style URL
+                center: userLocation.value!, // starting position [lng, lat]
+                zoom: 15, // starting zoom
+            });
+
+            new Mapboxgl.Popup({ 
+                offset: [ 0, -45 ],
+                closeButton: false, 
+            })
+            .setLngLat(userLocation.value!)
+            .setHTML(`<h5>¡Estoy aquí!</h5>
+                <p>Actualmente en San Ramón</p>`)
+            .addTo(map);
+
+            new Mapboxgl.Marker({
+                color: '#ef233c',
+                draggable: true
+            }).setLngLat(userLocation.value!)
+            .addTo(map);
+        }
+
         onMounted(() => {
-            console.log()
+            if ( isUserLocationReady.value ) 
+                return initMap();
+        });
+
+        watch( isUserLocationReady, ( newVal ) => {
+            if( isUserLocationReady.value )
+                initMap(); 
         })
 
         return {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import mapboxgl from 'mapbox-gl';
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFudWpzIiwiYSI6ImNsYTJydTR4dDBqdDQzcG8zbmR2ZG9xb3YifQ.0C-B4Z4qxtODQ0OdcPZlaQ';
+
 if( !navigator.geolocation ){
     alert( 'Geolocation no es soportado por el navegador' )
     throw new Error( 'Geolocation no es soportado por el navegador' );
